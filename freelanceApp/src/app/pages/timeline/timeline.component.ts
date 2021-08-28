32a28a954ff62c2b5d6f453a870595fd20e06c50
aya446 <ayaramadan7799@gmail.com>
2021-08-28 19:10:39 +0200

    ..

--- freelanceApp/src/app/pages/timeline/timeline.component.ts
+++ freelanceApp/src/app/pages/timeline/timeline.component.ts
@@ -11,7 +11,7 @@ export class TimelineComponent implements OnInit {
   constructor(private _user:UsersService) { 
 
     _user.getAllPosts().subscribe( 
-      (data) => {console.log(data); this.allPost = data} ,
+      (post) => {console.log(post); this.allPost = post.data} ,
       ( e ) => { console.log('error'); console.log(e); },
       ( ) => { console.log('then'); }
     )
